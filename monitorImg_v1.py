841713503be0454ad3a55a9bfb6199d83b9f1074
glydecovy <111717344+glydecovy@users.noreply.github.com>
2022-10-31 13:25:25 +0900

    Update monitorImg_v1.py

--- monitorImg_v1.py
+++ monitorImg_v1.py
@@ -25,7 +25,7 @@ def sendMsg(telegram_token, msgText):
 	bot.sendMessage(chat_id = cat_id, text=msgText)	
     
     
-if __name__ == '__main__':
+if __name__ == '__main__':	
 	telegram_token = sys.argv[1]
 	cat_id 	= sys.argv[2]
 	
@@ -59,118 +59,116 @@ if __name__ == '__main__':
 # 	error_flag : false 가 정상 = 모든 이미지가 3개 이상인 경우
 	error_flag = False 
 
-    soup = Soup(html)
-    divs = soup.find("div", {"class": "home-group"}, partial=True)
-    # [div.text for div in divs]
+	soup = Soup(html)
+	divs = soup.find("div", {"class": "home-group"}, partial=True)
+	# [div.text for div in divs]
 
-    for index, value in enumerate(divs):
-#         index 12, 글라이드스토리 제외
-        if index == 12 : continue
-#         print(index, value.text)
-#         print(dir(value))
-#         print(value.attrs)
-        tmp = value.attrs
+	for index, value in enumerate(divs):		
+		#index 12, 글라이드스토리 제외
+	        if index == 12 : continue
+
+		tmp = value.attrs
         
         ###########################################################################################################
-        grpNm = tmp.get('class')        
-        table = bs.find('div', { 'class': grpNm }) 
-        title = tmp.get('id')
-#         print("title:", title)
-    
-        tmp_table = bs.find('div', { 'class': grpNm })  
-        tmp_title = tmp_table.find('div', { 'class': 'group-head-copy' })
-        
-        try:
-            t_title = tmp_title.text
-# 값이 없으면
-        except Exception as e:
-            t_title = table.find('span', { 'class': 'group-head-copy' })   
-            try:
-                t_title = tmp_title.text   
-            except Exception as e:
-                t_title = title
-                
-        title = t_title
-        print("tmp_title:", t_title)
+		grpNm = tmp.get('class')        
+		table = bs.find('div', { 'class': grpNm }) 
+		title = tmp.get('id')
 
-        data = []                            # 데이터를 저장할 리스트 생성
-        result = []
-        if grpNm == 'home-group1':
-            tmp_img = table.find_all("img")
-            tmp_src = "src"
-        elif grpNm == 'home-group13-1':
-            tmp_img = table.find_all("img")
-            tmp_src = "data-src"
-        elif grpNm == 'home-group19':
-            tmp_img = table.find_all('div', { 'class': 'user-review-wrap' })
-            
-        elif grpNm == 'home-group13-2':
-            tmp_img = table.find_all("img")
-            tmp_src = "data-src"
-            
-        elif grpNm == 'home-group2':
-            tmp_img = table.find_all("img")
-            tmp_src = "src"
-            
-        elif grpNm == 'home-group18':
-            tmp_img = table.find_all("img")
-            tmp_src = "data-src"
-            
-        elif grpNm == 'home-group12':
-            tmp_img = table.find_all("img", { 'class': 'thumb-type1'})
-            tmp_src = "data-src"
-            
-        elif grpNm == 'home-group13':
-            tmp_img = table.find_all("img", { 'class': 'thumb-type1'})
-            tmp_src = "data-src"
-            
-        elif grpNm == 'home-group14':
-            tmp_img = table.find_all("img")
-            tmp_src = "data-src"
-            
-        elif grpNm == 'home-group7':
-            tmp_img = table.find_all("img", { 'class': 'thumb-type1'})
-            tmp_src = "data-src"
-            
-        elif grpNm == 'home-group11':
-            tmp_img = table.find_all("img", { 'class': 'thumb-type1'})
-            tmp_src = "data-src"      
-            
-        elif grpNm == 'home-group15':
-            tmp_img = table.find_all("img", { 'class': 'thumb-type1'})
-            tmp_src = "data-src"            
-            
-        else:
-            tmp_img = table.find_all("img")
-            tmp_src = "src"
+    
+		tmp_table = bs.find('div', { 'class': grpNm })  
+		tmp_title = tmp_table.find('div', { 'class': 'group-head-copy' })
+
+		try:
+			t_title = tmp_title.text
+# 값이 없으면
+		except Exception as e:
+			t_title = table.find('span', { 'class': 'group-head-copy' })   
+		try:
+			t_title = tmp_title.text   
+		except Exception as e:
+			t_title = title
+                
+		title = t_title
+		print("tmp_title:", t_title)
+
+		data = []                            # 데이터를 저장할 리스트 생성
+		result = []
+		if grpNm == 'home-group1':
+			tmp_img = table.find_all("img")
+			tmp_src = "src"
+		elif grpNm == 'home-group13-1':
+			tmp_img = table.find_all("img")
+			tmp_src = "data-src"
+		elif grpNm == 'home-group19':
+			tmp_img = table.find_all('div', { 'class': 'user-review-wrap' })
+
+		elif grpNm == 'home-group13-2':
+			tmp_img = table.find_all("img")
+			tmp_src = "data-src"
+
+		elif grpNm == 'home-group2':
+			tmp_img = table.find_all("img")
+			tmp_src = "src"
+
+		elif grpNm == 'home-group18':
+			tmp_img = table.find_all("img")
+			tmp_src = "data-src"
+
+		elif grpNm == 'home-group12':
+			tmp_img = table.find_all("img", { 'class': 'thumb-type1'})
+			tmp_src = "data-src"
+
+		elif grpNm == 'home-group13':
+			tmp_img = table.find_all("img", { 'class': 'thumb-type1'})
+			tmp_src = "data-src"
+
+		elif grpNm == 'home-group14':
+			tmp_img = table.find_all("img")
+			tmp_src = "data-src"
+
+		elif grpNm == 'home-group7':
+			tmp_img = table.find_all("img", { 'class': 'thumb-type1'})
+			tmp_src = "data-src"
+
+		elif grpNm == 'home-group11':
+			tmp_img = table.find_all("img", { 'class': 'thumb-type1'})
+			tmp_src = "data-src"      
+
+		elif grpNm == 'home-group15':
+			tmp_img = table.find_all("img", { 'class': 'thumb-type1'})
+			tmp_src = "data-src"            
+
+		else:
+			tmp_img = table.find_all("img")
+			tmp_src = "src"
         
         
-        if grpNm == 'home-group19':
-            for img in tmp_img : 
-                src = img.text
-                data.append([src])            
-        else :
-            
-            for img in tmp_img:      
-                src = img[tmp_src]    
-                data.append([src]) 
+		if grpNm == 'home-group19':
+			for img in tmp_img : 
+				src = img.text
+				data.append([src])            
+		else :
+
+			for img in tmp_img:      
+				src = img[tmp_src]    
+				data.append([src]) 
         
         # 중복제거2
-        for value in data:
-            if value not in result:result.append(value)
-        summary.append([title, len(result)]) 
+		for value in data:
+			if value not in result:result.append(value)
+		summary.append([title, len(result)]) 
 
-    print("###########################################################################################################")
-    print(summary)
+	print("###########################################################################################################")
+	print(summary)
 
-    df = pd.DataFrame(summary, columns=['corner', 'image_count'])
-    df.set_index('corner',drop=True,inplace=True)
-#     print(df)
-    df.reset_index(inplace=True)
-    sDf = sendingForm(df)
-    t= tabulate(df, headers='keys', tablefmt='plain', showindex=False, numalign='right' )
-    print(t)
-    try:
-      sendMsg(telegram_token, t)
-    except Exception as e:
-      print("e:",e)   
+	df = pd.DataFrame(summary, columns=['corner', 'image_count'])
+	df.set_index('corner',drop=True,inplace=True)
+
+	df.reset_index(inplace=True)
+	sDf = sendingForm(df)
+	t= tabulate(df, headers='keys', tablefmt='plain', showindex=False, numalign='right' )
+	print(t)
+	try:
+		sendMsg(telegram_token, t)
+	except Exception as e:
+		print("e:",e)   
